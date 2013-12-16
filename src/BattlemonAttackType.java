3e528e88da4fc49e034df647259e269267839bdc
David Chavez <david@psychosity.net>
2013-12-16 01:59:09 -0600

    Deleted stuff

--- src/BattlemonAttackType.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public class BattlemonAttackType
-{
-	String name;
-	ArrayList<BattlemonAttackType> strengths = new ArrayList<BattlemonAttackType>();
-	ArrayList<BattlemonAttackType> weaknesses = new ArrayList<BattlemonAttackType>();
-}
